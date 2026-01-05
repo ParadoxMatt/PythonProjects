875898c56ef408496887b9f2123716b2dacfb55a
Matthew Chihata <chihatamatthew@gmail.com>
2026-01-05 17:19:39 +0200

    Additions
    Added  new files to showcase what I learnt so far
    
    most are just dumb games but they will get better as I use python more

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,28 @@
 #hello world
+#Word document is there. Name 
 
-def variablePractice():
-    num = 123
-    print(num)
 
-variablePractice()
+print("Hello World!")
+
+#Variables
+
+string_variable = ("This is a string")
+print(f"What is a string ? " + string_variable)
+
+Integer_variable = (1)
+print(f"You are always number " + str(Integer_variable))
+
+float_variable = (3.14159)
+print(f"Pi is an example of float. The valure of pi is: "+str(float_variable))
+
+boolean_variable = True
+print(boolean_variable)
+
+#Input and verification using if statements
+answer = input("What is the numerical value of Pi: ")
+if answer == str(float_variable):
+    print("Correct")
+else:
+    print("Incorrect")
+
+# This is good but it does not take into account any other invalid options. 
