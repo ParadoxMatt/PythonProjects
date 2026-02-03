46ce42ebbef260e5023f66010f7946d8ce7d6fa0
Matthew Chihata <chihatamatthew@gmail.com>
2026-02-03 15:12:15 +0200

    Updated code to the game
    Game functions properly now
    Game loop works the score system does not though

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,52 +1,61 @@
-import random
+import random 
+global score 
 
-#List of tools to use during the game
-tool_list = ["rock","paper", "scissors",]
-userInput = input("Press N to quit, press Y to play: ")
 
-if userInput == "y":
-    print("Pick a tool to use! or press 'Q' to quit")
-    usertool = input("1. Rock \n2. Paper \n3. Scissors\n ")
+# 1. make score variable accessible in the roshambo function
+# 2. 
 
-    while usertool.upper() != "Q":
-        pctool = random.choice(tool_list)
-        print(pctool)
-        if usertool == "1":
-            print("You picked rock")
-            if pctool == "rock":
-                print("You have drawn")
-            elif pctool == "paper":
-                print("Youve lost")
-            else:
-                print("WiN")
+weaponwheel = ("Rock","Paper", "Scissors")
+score = 0
+win_var = False
+
+def tutorial():
+    print("Hello, Welcome to rock, paper, scissors, shoot!")
+
+def play():
+    play_time = ""  # Initialize play_time variable
+    while play_time.strip().capitalize() != "No":
+        play_time = input("Let's play! Type 'No' to quit: ").strip().capitalize()
+        if play_time == "No":
+            print("Thanks for playing!")
+            print(f"Your final score is: {score}")
             break
 
-        if usertool == "2":
-            print("You picked paper")
-            if pctool == "paper":
-                print("You have drawn")
-            elif pctool == "scissors":
-                print("Youve lost")
-            else:
-                print("WiN")
-            break
+        print(f"Pick your weapon: {weaponwheel}")
+        user_weapon = ""
+        while user_weapon not in weaponwheel:
+            user_weapon = input("Which weapon do you choose to wield: ").capitalize()
 
-        if usertool == "3":
-            print("You picked scissors")
-            if pctool == "scissors":
-                print("You have drawn")
-            elif pctool == "rock":
-                print("Youve lost")
-            else:
-                print("WiN")
-            break
+        opp_weapon = random.choice(weaponwheel)
+        roshambo(user_weapon, opp_weapon) 
 
-    print("Pick a tool to use! or press 'Q' to quit")
-    usertool = input("1. Rock \n2. Paper \n3. Scissors\n ")
+        
 
-    userInput = input("Press N to quit, press Y to play: ")
+def roshambo(user_weapon, opp_weapon):
+    #Basic game mechanics using non global attributes from the play mechanic
+    print(f"You picked: {user_weapon}")
+    print(f"Opponent picked: {opp_weapon}")
 
-elif userInput != "n":
-     print("Please enter a valid letter")
-else:
-    print("Thanks for playing!")
+    if user_weapon == opp_weapon:
+        print("It is a tie draw again!")
+    elif user_weapon == "Rock" and opp_weapon =="Paper":
+        print("YOU LOSE!")
+    elif user_weapon == "Scissors" and opp_weapon =="Rock":
+        print("YOU LOSE!")
+    elif user_weapon == "Paper" and opp_weapon =="Scissors":
+        print("YOU LOSE!")
+    else:
+        print("YOU WIN!")
+        win_var = True
+        return win_var 
+
+print(win_var)
+
+def score():
+    score = 0
+    if win_var == True:
+        score += 1
+        return score 
+
+
+play()
